716bfdb47b6926b0682e3dff2dc3ddf58f58d0c2
Kevin Dat Nguyen <datkevin@gmail.com>
2019-11-21 16:28:42 -0800

    Finished seeding database

--- seeds-database/pokemonmoves-seed.js
+++ seeds-database/pokemonmoves-seed.js
@@ -4,7 +4,83 @@ let database = require("./database/database.js")
 let pokemonmoves = [
 	{
 		pokemonID: 1,
+		moveID: 13,
+	},
+	{
+		pokemonID: 1,
+		moveID: 11,
+	},
+	{
+		pokemonID: 1,
+		moveID: 12,
+	},
+	{
+		pokemonID: 1,
+		moveID: 4,
+	},
+	{
+		pokemonID: 2,
+		moveID: 7,
+	},
+	{
+		pokemonID: 2,
 		moveID: 9,
+	},
+	{
+		pokemonID: 2,
+		moveID: 8,
+	},
+	{
+		pokemonID: 2,
+		moveID: 6,
+	},
+	{
+		pokemonID: 3,
+		moveID: 7,
+	},
+	{
+		pokemonID: 3,
+		moveID: 8,
+	},
+	{
+		pokemonID: 3,
+		moveID: 10,
+	},
+	{
+		pokemonID: 3,
+		moveID: 9,
+	},
+	{
+		pokemonID: 4,
+		moveID: 2,
+	},
+	{
+		pokemonID: 4,
+		moveID: 5,
+	},
+	{
+		pokemonID: 4,
+		moveID: 15,
+	},
+	{
+		pokemonID: 4,
+		moveID: 4,
+	},
+	{
+		pokemonID: 5,
+		moveID: 1,
+	},
+	{
+		pokemonID: 5,
+		moveID: 3,
+	},
+	{
+		pokemonID: 5,
+		moveID: 6,
+	},
+	{
+		pokemonID: 5,
+		moveID: 16,
 	},
 ]
 
---
+++ seeds-database/pokemontypes-seed.js
@@ -0,0 +1,50 @@
+//Requires database
+let database = require("./database/database.js")
+
+let pokemontypes = [
+	{
+		pokemonID: 1,
+		typeID: 14,
+	},
+	{
+		pokemonID: 2,
+		typeID: 4,
+	},
+	{
+		pokemonID: 3,
+		typeID: 6,
+	},
+	{
+		pokemonID: 4,
+		typeID: 5,
+	},
+	{
+		pokemonID: 4,
+		typeID: 9,
+	},
+	{
+		pokemonID: 5,
+		typeID: 8,
+	},
+	{
+		pokemonID: 5,
+		typeID: 9,
+	},
+]
+
+//queries for intializing seeds
+let clearTable = "DELETE FROM pokemontypes"
+let insertSeed = "INSERT INTO pokemontypes VALUES (?, ?)"
+
+//Adds seed to database
+database.run(clearTable, (error) => {
+	if (error) console.log(new Error("Could not clear table"), error)
+	else {
+		pokemontypes.forEach((poketype) => {
+			database.run(insertSeed, [poketype.pokemonID, poketype.typeID], (error) => {
+				if (error) console.log(new Error("Could not add seed abilities", error))
+				else console.log(poketype.pokemonID + " has been added successfully")
+			})
+		})
+	}
+})
